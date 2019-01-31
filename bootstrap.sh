c8d384c0eff9773a8893670397c42ccc68c64014
Lattapon Yodsuwan <clozed2u@gmail.com>
2019-02-01 00:47:21 +0700

    Update bootstrap script and vim, zsh, tmux config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,46 @@
 #!/usr/bin/env bash
 
-function installHomebrew() {
+function install_homebrew() {
   bash -c 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
   brew bundle install --file=$PWD/dot-brewrc
 }
 
-function linkGit() {
+function symlink_git() {
   rm -rf $HOME/.config/git
   mkdir -p $HOME/.config/git
   ln -s $PWD/dot-gitignore $HOME/.config/git/ignore
   ln -s $PWD/dot-gitconfig $HOME/.config/git/config
 }
 
-function makeVimDir() {
-  mkdir -p $HOME/.vim/{autoload,backup,bundle,colors,tmp,undo}
+function make_vim_dir() {
+  mkdir -p $HOME/.vim/{autoload,backup,bundle,colors,tmp,undo,pack}
 }
 
-function linkAll() {
+function symlink_vim_to_neovim() {
+  mkdir -p $HOME/.config
+  ln -s $HOME/.vim $HOME/.config/nvim
+  ln -s $PWD/dot-vimrc $HOME/.config/nvim/init.vim
+}
+
+function connect_the_dots() {
   for d in dot-*; do
     dotname=${d/dot-/}
     rm -rf $HOME/.$dotname
     ln -s $PWD/$d $HOME/.$dotname
   done
 
-  linkGit
-  makeVimDir
+  symlink_git
+  make_vim_dir
+  symlink_vim_to_neovim
 }
 
 if [ ! which brew > /dev/null 2>&1 ]; then
-  installHomebrew
+  install_homebrew
 fi
 
 read -p "This will overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo ""
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  linkAll
+if [ $REPLY =~ ^[Yy]$ ]; then
+  connect_the_dots
 fi
-
