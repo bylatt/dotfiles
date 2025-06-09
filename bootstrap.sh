b37ac860f28300d27f988ac4f91ec190b9cc070d
Lattapon Yodsuwan <lattapon@bylatt.co>
2025-06-09 20:31:58 +0700

    feat: update installation script

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,15 +13,19 @@ function connect_the_dots() {
   done
 }
 
-if [ ! which brew > /dev/null 2>&1 ]; then
-  install_homebrew
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  if [[ ! which brew ]] >/dev/null 2>&1; then
+    install_homebrew
+  fi
 fi
 
 read -p "This will overwrite existing files in your home directory. Are you sure? (y/n) " reply
 echo ""
 
 case $reply in
-  [Yy]* ) connect_the_dots;;
-  [Nn]* ) exit;;
-  * ) echo "Yes or No only";;
+[Yy]*) connect_the_dots ;;
+[Nn]*) exit ;;
+*) echo "Yes or No only" ;;
 esac
